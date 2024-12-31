175b3382d25e15652caf06135f951d8c392b536c
kmurdhar <murdhariya.kamlesh3@gmail.com>
2024-12-31 12:19:13 +0530

    backend for ecommerce

---
+++ dream-shops-third-commit-with-modifications/src/main/java/com/dailycodework/dreamshops/controller/AuthController.java
@@ -0,0 +1,105 @@
+package com.dailycodework.dreamshops.controller;
+
+
+import com.dailycodework.dreamshops.data.RoleRepository;
+import com.dailycodework.dreamshops.dto.UserDto;
+import com.dailycodework.dreamshops.model.Role;
+import com.dailycodework.dreamshops.model.User;
+import com.dailycodework.dreamshops.repository.UserRepository;
+import com.dailycodework.dreamshops.request.LoginRequest;
+import com.dailycodework.dreamshops.response.ApiResponse;
+import com.dailycodework.dreamshops.response.JwtResponse;
+import com.dailycodework.dreamshops.security.jwt.JwtUtils;
+import com.dailycodework.dreamshops.security.user.ShopUserDetails;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/auth")
+public class AuthController {
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse> login(@Valid @RequestBody LoginRequest request) {
+        try {
+            Authentication authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(
+                            request.getEmail(), request.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            String jwt = jwtUtils.generateTokenForUser(authentication);
+            ShopUserDetails userDetails = (ShopUserDetails) authentication.getPrincipal();
+            JwtResponse jwtResponse = new JwtResponse(userDetails.getId(), jwt);
+            return ResponseEntity.ok(new ApiResponse("Login Success!", jwtResponse));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ApiResponse(e.getMessage(), null));
+        }
+
+    }
+    
+    
+    
+    @PostMapping("/register")
+	public ResponseEntity<Map<String, Object>> registerHandler(@Valid @RequestBody UserDto userDto) throws UsernameNotFoundException {
+    	 Role userRole = roleRepository.findByName("ROLE_USER").get();
+    	String encodedPass = passwordEncoder.encode(userDto.getPassword());
+    	User user = new User();
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(encodedPass);
+        user.setRoles(Set.of(userRole));
+        userRepository.save(user);
+       
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(
+                		userDto.getEmail(), userDto.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateTokenForUser(authentication);
+        ShopUserDetails userDetails = (ShopUserDetails) authentication.getPrincipal();
+        JwtResponse token = new JwtResponse(userDetails.getId(), jwt);
+        
+		return new ResponseEntity<Map<String, Object>>(Collections.singletonMap("jwt-token", token),
+				HttpStatus.CREATED);
+	}
+
+	/*
+	 * @PostMapping("/login") public Map<String, Object>
+	 * loginHandler(@Valid @RequestBody LoginRequest credentials) {
+	 * 
+	 * UsernamePasswordAuthenticationToken authCredentials = new
+	 * UsernamePasswordAuthenticationToken( credentials.getEmail(),
+	 * credentials.getPassword());
+	 * 
+	 * authenticationManager.authenticate(authCredentials);
+	 * 
+	 * String token = jwtUtil.generateToken(credentials.getEmail());
+	 * 
+	 * return Collections.singletonMap("jwt-token", token); }
+	 */
+    
+    
+    
+}
